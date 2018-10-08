757144165b2a5e29941add99931e57b130e379b6
mooninsky1 <moon_in_sky@sina.cn>
2018-10-08 15:44:38 +0800

    update
    update

--- src/sqlserver/sqlserver.h
+++ src/sqlserver/sqlserver.h
@@ -16,12 +16,23 @@ protected:
 	void ExecuteSP();
 	void ExecuteSP1();
 	void ExecuteSP2();
+	//lihf 2018/09/30 测试返回数据集
+	void ExecuteSP3();
 	void ExecuteRecordsetSQL();
 	void ExecuteCommandSQL();
 private:
 	//清除存储过程绑定的参数
 	bool DestroyParams();
 	bool BindParams(LPCSTR pszName, DataTypeEnum enumDataType,  ParameterDirectionEnum enumDirType,int nSize, char * pszData);
+	//lihf 2018/09/30 取得存储过程返回值跟output参数
+	void GetReturnValueSP();
+		/** 解析数据库返回值
+	@param   
+	@param   
+	@return  
+	*/
+	bool					FetchDBRetValue(_variant_t& varSource, LPSTR pszValue, int nLen);
+
 private:
 	////////////////////////数据库对像有关//////////////////////////////
 	// COM _Command 命令对象
@@ -32,5 +43,11 @@ private:
 
 	// COM _Connection 连接对象
 	_ConnectionPtr			m_compConnection;
+
+	// 返回代码
+	int						m_nDBRetCode;
+
+	// 返回描述
+	char					m_szDBRetDesc[30];
 
 };
